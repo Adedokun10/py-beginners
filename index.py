c0329913343cabf030222fadabd547013efd6bb5
Adedokun10 <158764355+Adedokun10@users.noreply.github.com>
2024-02-03 09:11:28 -0800

    Add files via upload

---
+++ index.py
@@ -0,0 +1,57 @@
+
+# variables with number data type#
+a=10
+b=20
+a=50
+c=a+b
+print(c)
+# with String datatype#
+name="Tola"
+Surname="Adedokun"
+#Surname="Emeka"
+fullName=name+" "+Surname
+print(fullName)
+#IF ELSE STATEMENT
+"""A=7
+B=10
+A=input("Enter number here \t")
+print("A is "+A)
+B=input("Enter second number \t")
+print("B is "+B)
+if A>B : print("A is greater than B")
+else:
+     print("B is greater than A")
+
+Nationality="Nigeria"
+Age=18 
+Nation=input("Enter your nationality")
+if Nation==Nationality: print("enter age")
+age=input("input age")
+
+if int(age) >=int(Age):print("you are eligible to vote, please vote wisely")
+else: print("you are ineligible, because you are not up to 18")
+
+#program to check if a number is divisible 2 and 5  if a number is divisible by 2 and not 5 if a number is divisible by 5 and 2 and if number is not divisible by both 2 and 5 Goal:to learn operaors
+num=12
+num=input("enter a number")
+if float(num)%2==0 and float(num)%5==0:print(num+"is divisible by both 2 and 5")
+else:
+    if float(num)%2==0 and float(num)%5!=0:print(num+"is divisible by 2 and not 5")
+    else:
+        if float(num)%2!=0 and float(num)%5==0:print(num+"is not divisible by 2 and but by 5 ")
+        """
+x=["apple","orange", 5]
+print(x)
+fruits=["Orange", "pineapple","Mango"]
+for x in fruits:
+  print(x)
+fruits=["Orange", "pineapple","Mango"]
+"""=0
+while x<len(fruits):
+    print(fruits[x])
+    x+=1
+    a=len(fruits)
+    print(a)"""
+c=fruits[2]
+print("The position of two is"+" "+c)
+
